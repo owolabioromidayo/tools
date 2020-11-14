30e4a964514f6ac13a383dad8c207005b8016c9f
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-11-14 19:39:36 +0100

    .

---
+++ free_wsl.bat
@@ -0,0 +1,6 @@
+wsl --shutdown
+(echo select vdisk file="C:\Users\Oromidayo\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows_79rhkp1fndgsc\LocalState\ext4.vhdx"
+echo compact vdisk
+echo exit
+) | diskpart
+
